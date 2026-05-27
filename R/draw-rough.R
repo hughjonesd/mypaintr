acf1a5cef20ed76caa3f80ef9ece6496e2414097
David Hugh-Jones <davidhughjones@gmail.com>
2026-05-27 23:16:40 +0100

    Refactor mypaint rough shape drawing

--- R/draw-rough.R
+++ R/draw-rough.R
@@ -499,6 +499,39 @@ warn_missing_fill_for_pattern <- function(fill_pattern, col) {
   }
 }
 
+draw_mypaintr_rough_shape <- function(primitive, shape_args, paths, hand_spec,
+                                      fill_pattern, col, border, rule = NULL, ...) {
+  dots <- list(...)
+
+  state <- current_device_style()
+  on.exit(apply_device_style_state(state), add = TRUE)
+  invisible(.Call(mypaintr_device_set_hand, hand_spec, hand_spec, TRUE, TRUE))
+
+  neutral_hand <- hand(seed = hand_spec$seed)
+  invisible(with_hand_seed(neutral_hand$seed, {
+    if (is.null(fill_pattern)) {
+      if (is_visible_col(col) || is_visible_col(border)) {
+        do.call(primitive, c(shape_args, list(col = col, border = border), dots))
+      }
+    } else {
+      if (is_visible_col(col)) {
+        fill_args <- c(
+          list(paths = paths, hand_spec = neutral_hand, col = col, fill_pattern = fill_pattern),
+          dots
+        )
+        if (!is.null(rule)) {
+          fill_args$rule <- rule
+        }
+        do.call(draw_rough_fill_pattern, fill_args)
+      }
+      if (is_visible_col(border)) {
+        do.call(primitive, c(shape_args, list(col = NA, border = border), dots))
+      }
+    }
+    NULL
+  }))
+}
+
 #' @rdname rough_polypath
 #' @inheritParams mypaintr-rough-fill
 #' @param ... Graphics parameters passed to [graphics::lines()].
@@ -523,47 +556,18 @@ draw_rough_polypath <- function(x, y = NULL, id = NULL, rule = c("winding", "eve
   warn_missing_fill_for_pattern(fill_pattern, col)
 
   if (is_mypaintr_device()) {
-    state <- current_device_style()
-    on.exit(apply_device_style_state(state), add = TRUE)
-    invisible(.Call(mypaintr_device_set_hand, hand_spec, hand_spec, TRUE, TRUE))
-
-    neutral_hand <- hand(seed = hand_spec$seed)
-    return(invisible(with_hand_seed(neutral_hand$seed, {
-      if (is.null(fill_pattern)) {
-        if (is_visible_col(col) || is_visible_col(border)) {
-          solid_path <- join_polypath_na(paths0)
-          do.call(
-            graphics::polypath,
-            c(
-              list(x = solid_path$x, y = solid_path$y, rule = rule, col = col, border = border),
-              list(...)
-            )
-          )
-        }
-      } else {
-        if (is_visible_col(col)) {
-          draw_rough_fill_pattern(
-            paths0,
-            neutral_hand,
-            col = col,
-            fill_pattern = fill_pattern,
-            rule = rule,
-            ...
-          )
-        }
-        if (is_visible_col(border)) {
-          solid_path <- join_polypath_na(paths0)
-          do.call(
-            graphics::polypath,
-            c(
-              list(x = solid_path$x, y = solid_path$y, rule = rule, col = NA, border = border),
-              list(...)
-            )
-          )
-        }
-      }
-      NULL
-    })))
+    solid_path <- join_polypath_na(paths0)
+    return(draw_mypaintr_rough_shape(
+      graphics::polypath,
+      list(x = solid_path$x, y = solid_path$y, rule = rule),
+      paths0,
+      hand_spec,
+      fill_pattern,
+      col,
+      border,
+      rule = rule,
+      ...
+    ))
   }
 
   invisible(with_hand_seed(hand_spec$seed, {
@@ -627,44 +631,16 @@ draw_rough_polygons <- function(x, y = NULL, hand = NULL, col = NA, border = gra
   warn_missing_fill_for_pattern(fill_pattern, col)
 
   if (is_mypaintr_device()) {
-    state <- current_device_style()
-    on.exit(apply_device_style_state(state), add = TRUE)
-    invisible(.Call(mypaintr_device_set_hand, hand_spec, hand_spec, TRUE, TRUE))
-
-    neutral_hand <- hand(seed = hand_spec$seed)
-    return(invisible(with_hand_seed(neutral_hand$seed, {
-      if (is.null(fill_pattern)) {
-        if (is_visible_col(col) || is_visible_col(border)) {
-          do.call(
-            graphics::polygon,
-            c(
-              list(x = xy$x, y = xy$y, col = col, border = border),
-              list(...)
-            )
-          )
-        }
-      } else {
-        if (is_visible_col(col)) {
-          draw_rough_fill_pattern(
-            list(list(x = xy$x, y = xy$y)),
-            neutral_hand,
-            col = col,
-            fill_pattern = fill_pattern,
-            ...
-          )
-        }
-        if (is_visible_col(border)) {
-          do.call(
-            graphics::polygon,
-            c(
-              list(x = xy$x, y = xy$y, col = NA, border = border),
-              list(...)
-            )
-          )
-        }
-      }
-      NULL
-    })))
+    return(draw_mypaintr_rough_shape(
+      graphics::polygon,
+      list(x = xy$x, y = xy$y),
+      list(list(x = xy$x, y = xy$y)),
+      hand_spec,
+      fill_pattern,
+      col,
+      border,
+      ...
+    ))
   }
 
   invisible(with_hand_seed(hand_spec$seed, {
